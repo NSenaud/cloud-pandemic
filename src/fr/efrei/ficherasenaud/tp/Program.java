c39f4f7d63eb7d92a43108b7d7ce428a238c07ad
Nicolas Senaud <nicolas@senaud.fr>
2014-11-16 21:51:06 +0100

    Improve Main Program With Some Tests

--- src/fr/efrei/ficherasenaud/tp/Program.java
+++ src/fr/efrei/ficherasenaud/tp/Program.java
@@ -12,15 +12,40 @@ public class Program {
 	public static void main(String[] arguments) {
 		City villejuif = new City();
 		
-		Inhabitant jerome = new Inhabitant("Jerome");
-		
+		System.out.format("================== Add Jérôme =================\n");
+		Inhabitant jerome = new Inhabitant("Jérôme");
 		villejuif.addInhabitant(jerome);
+		displayStatistics(villejuif);
 		
+		System.out.format("================ Initialisation ===============\n");
 		for (int i = 0; i < 50; i++) {
 			villejuif.addInhabitant(new Inhabitant("Unknown"));
 		}
 		
+		displayStatistics(villejuif);
+		
+		System.out.format("================ Infect Jérôme ================\n");
+		villejuif.infect(jerome);
+		displayStatistics(villejuif);
+		System.out.format("========== Put Jérôme in Quarantined ==========\n");
 		villejuif.putInQuarantine(jerome);
+		displayStatistics(villejuif);
+		System.out.format("================= Heal Jérôme =================\n");
 		villejuif.heal(jerome);
+		displayStatistics(villejuif);
+		System.out.format("================ Infect Jérôme ================\n");
+		villejuif.infect(jerome);
+		displayStatistics(villejuif);
+		System.out.format("================= Kill Jérôme =================\n");
+		villejuif.kill(jerome);
+		displayStatistics(villejuif);
+	}
+	
+	private static void displayStatistics(City city) {
+		System.out.format("==================== Stats ====================\n");
+		System.out.format("Alive: \t%d\n", city.getAliveInhabitants());
+		System.out.format("Infected: \t%d\n", city.getInfectedInhabitants());
+		System.out.format("Quarantined: \t%d\n", city.getQuarantinedInhabitants());
+		System.out.format("Died: \t%d\n", city.getInhabitantsDead());
 	}
 }
