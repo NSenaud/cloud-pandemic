e89cb4a3f6ce82e5bafaa9a756abdb87d73448c0
Nicolas Senaud <nicolas@senaud.fr>
2014-11-16 17:58:50 +0100

    Comments City Class

--- src/fr/efrei/ficherasenaud/tp/City.java
+++ src/fr/efrei/ficherasenaud/tp/City.java
@@ -2,26 +2,42 @@ package fr.efrei.ficherasenaud.tp;
 
 import java.util.ArrayList;
 
+/**
+ * @class City
+ * 
+ *  A City contains inhabitants. "It" may kill infected inhabitants, put them
+ *  in quarantine, and heal them if they are in quarantine.
+ */
+
 public class City {
 	private ArrayList<Inhabitant> inhabitantsList;
 	private ArrayList<Inhabitant> infectedInhabitantsList;
 	private ArrayList<Inhabitant> quarantainedInhabitantsList;
 	
-	// Constructor
 	public City() {
+		/// Initialize  Lists
 		inhabitantsList = new ArrayList<Inhabitant>();
 		infectedInhabitantsList = new ArrayList<Inhabitant>();
 		quarantainedInhabitantsList = new ArrayList<Inhabitant>();
 	}
 	
+	/**
+	 * @param inhabitant An infected inhabitant
+	 */
 	public void addInhabitant(Inhabitant inhabitant) {
 		this.inhabitantsList.add(inhabitant);
 	}
 	
+	/**
+	 * @param inhabitant An infected inhabitant
+	 */
 	public void kill(Inhabitant inhabitant) {
 		die(inhabitant);		
 	}
 	
+	/**
+	 * @param inhabitant An infected inhabitant
+	 */
 	public void die(Inhabitant inhabitant) {
 		this.inhabitantsList.remove(inhabitant);
 		
@@ -37,6 +53,12 @@ public class City {
 //		println("")
 	}
 	
+	/**
+	 * An Inhabitant infects another one.
+	 * 
+	 * @param inhabitantSource An infected inhabitant
+	 * @param inhabitantTarget A non infected inhabitant
+	 */
 	public void infect(Inhabitant inhabitantSource, Inhabitant inhabitantTarget) {
 		if (inhabitantSource.getInfected()) {
 			inhabitantTarget.setInfected(true);
@@ -44,11 +66,21 @@ public class City {
 		}
 	}
 	
+	/**
+	 * The City may arbitrary infects an inhabitant.
+	 * 
+	 * @param inhabitant A non infected inhabitant
+	 */
 	public void infect(Inhabitant inhabitant) {
 		inhabitant.setInfected(true);
 		this.infectedInhabitantsList.add(inhabitant);
 	}
 	
+	/**
+	 * The City may heal an infected inhabitant.
+	 * 
+	 * @param inhabitant An infected inhabitant
+	 */
 	public void heal(Inhabitant inhabitant) {
 		if (inhabitant.getQuarantined())
 		{
@@ -59,6 +91,11 @@ public class City {
 		}
 	}
 	
+	/**
+	 * The City may put in quarantine an infected inhabitant.
+	 * 
+	 * @param inhabitant An infected inhabitant
+	 */
 	public void putInQuarantine(Inhabitant inhabitant) {
 		inhabitant.setQuarantined(true);
 		this.quarantainedInhabitantsList.add(inhabitant);
