a6cf33ea47c521a456861d2f1385b09e12292443
Nicolas Senaud <nicolas@senaud.fr>
2014-11-16 18:08:07 +0100

    Comment Inhabitant Class

--- src/fr/efrei/ficherasenaud/tp/Inhabitant.java
+++ src/fr/efrei/ficherasenaud/tp/Inhabitant.java
@@ -1,39 +1,73 @@
 package fr.efrei.ficherasenaud.tp;
 
+/**
+ * 
+ * @class Inhabitant
+ * 
+ * Inhabitant Class represent a single inhabitant of a City Class.
+ *
+ */
 public class Inhabitant {
-	
 	private boolean infected;
 	private boolean quarantined;
 	private String name;
 	
+	/**
+	 * 
+	 * @param name The inhabitant name, useful for UI and Debug, don't need to
+	 * be unique.
+	 */
 	public Inhabitant(String name) {
 		this.infected = false;
 		this.quarantined = false;
 		this.name = name;
 	}
 	
+	/**
+	 * 
+	 * @return Inhabitant's infection state 
+	 */
 	public boolean getInfected() {
 		return this.infected; 
 	}
 
+	/**
+	 * 
+	 * @param infected Inhabitant's infection state
+	 */
 	public void setInfected(boolean infected) {
 		this.infected = infected;
 	}
 	
+	/**
+	 * 
+	 * @return Inhabitant's quarantine state
+	 */
 	public boolean getQuarantined() {
 		return this.quarantined;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	/**
+	 * 
+	 * @param quarantined Inhabitant's quarantine state
+	 */
+	public void setQuarantined(boolean quarantined) {
+		this.quarantined = quarantined; 
 	}
 	
+	/**
+	 * 
+	 * @return Inhabitant's name
+	 */
 	public String getName() {
 		return this.name;
 	}
 	
-	public void setQuarantined(boolean quarantined) {
-		this.quarantined = quarantined; 
+	/**
+	 * 
+	 * @param name Inhabitant's name
+	 */
+	public void setName(String name) {
+		this.name = name;
 	}
 }
-	
