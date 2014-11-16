36fe7ecbe22a40bd105d3e7b54434dad98be09fe
Nicolas Senaud <nicolas@senaud.fr>
2014-11-16 18:09:49 +0100

    Comments Program Class

--- src/fr/efrei/ficherasenaud/tp/Program.java
+++ src/fr/efrei/ficherasenaud/tp/Program.java
@@ -1,5 +1,13 @@
 package fr.efrei.ficherasenaud.tp;
 
+/**
+ * 
+ * @class Program
+ * 
+ * Main program class: initialize City Class and Inhabitants Classes to add to 
+ * City.
+ *
+ */
 public class Program {
 	public static void main(String[] arguments) {
 		City villejuif = new City();
