e4fffda81cedc99c7cb254c412d96a3d33285984
Nicolas Senaud <nicolas@senaud.fr>
2015-01-07 11:17:27 +0100

    Clean EmigrationEvent

--- src/fr/efrei/ficherasenaud/tp/EmigrationEvent.java
+++ src/fr/efrei/ficherasenaud/tp/EmigrationEvent.java
@@ -9,32 +9,42 @@ import fr.efrei.paumier.common.time.Event;
 public class EmigrationEvent implements Event {
 	private Duration duration;
 	private City sourceCity;
-	private ForeignCity targetCity;
 	private BaseRemoteCityBorder remote;
 	private Server host;
 	private Available available;
-	private Engine engine;
 	
 	private Inhabitant inhabitant;
 
 	public EmigrationEvent(Engine engine) {
 		this.duration = Parameters.immigrationWaitDuration;
 		this.sourceCity = Parameters.city;
-		this.targetCity = new ForeignCity();
 		
 		this.host = Parameters.server;
-		this.engine = engine;
 		this.available = new Available();
 		this.remote = new Remote(host, engine, available);
 	}
 
 	@Override
 	public void trigger() {
-		/// Select Inhabitant
+		/**
+		 * Select Inhabitant.
+		 */
 		ArrayList<Inhabitant> immigrationCandidats = new ArrayList<>();
 		immigrationCandidats.addAll(this.sourceCity.getAliveInhabitantsArray());
 		immigrationCandidats.removeAll(this.sourceCity.getQuarantinedInhabitantsArray());
 		this.inhabitant = this.sourceCity.selectAmong(immigrationCandidats);
+		
+		/**
+		 * Launch emigration.
+		 */
+		boolean success = this.remote.trySendingEmigrant(this.inhabitant.getInfected());
+		
+		if (success == true) {
+			System.out.println("Emigrant sent");
+		}
+		else {
+			System.out.println("Failed to send emigrant");
+		}
 	}
 
 	@Override
