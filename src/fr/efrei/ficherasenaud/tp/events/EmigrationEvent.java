b66fee085a7de3cc8386dde57c29c83e591429d2
Nicolas Senaud <nicolas@senaud.fr>
2015-01-08 08:45:50 +0100

    Create Remote at the Right Place

--- src/fr/efrei/ficherasenaud/tp/events/EmigrationEvent.java
+++ src/fr/efrei/ficherasenaud/tp/events/EmigrationEvent.java
@@ -7,9 +7,6 @@ import fr.efrei.ficherasenaud.tp.City;
 import fr.efrei.ficherasenaud.tp.Engine;
 import fr.efrei.ficherasenaud.tp.Inhabitant;
 import fr.efrei.ficherasenaud.tp.Parameters;
-import fr.efrei.ficherasenaud.tp.network.Available;
-import fr.efrei.ficherasenaud.tp.network.Remote;
-import fr.efrei.ficherasenaud.tp.network.Server;
 import fr.efrei.paumier.common.networking.BaseRemoteCityBorder;
 import fr.efrei.paumier.common.time.Event;
 
@@ -17,18 +14,12 @@ public class EmigrationEvent implements Event {
 	private Duration duration;
 	private City sourceCity;
 	private BaseRemoteCityBorder remote;
-	private Server host;
-	private Available available;
 	
 	private Inhabitant inhabitant;
 
 	public EmigrationEvent(Engine engine) {
 		this.duration = Parameters.emigrationWaitDuration;
 		this.sourceCity = Parameters.city;
-		
-		this.host = Parameters.server;
-		this.available = new Available();
-		this.remote = new Remote(host, engine, available);
 	}
 
 	@Override
--- src/fr/efrei/ficherasenaud/tp/network/Server.java
+++ src/fr/efrei/ficherasenaud/tp/network/Server.java
@@ -1,49 +1,25 @@
 package fr.efrei.ficherasenaud.tp.network;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.List;
 
-import fr.efrei.paumier.common.networking.MessageChannel;
-import fr.efrei.paumier.common.networking.MessageChannelHost;
+import fr.efrei.ficherasenaud.tp.Parameters;
 import fr.efrei.paumier.common.networking.MultipointMessageNode;
 
-public class Server implements MessageChannelHost {
+public class Server {
 	MultipointMessageNode multipointServer;
 	
-	private int port;
+	private Available available;
+	private Remote remote;
 
 	public Server(int port) {
-		this.port = port;
-		
 		try {
 			this.multipointServer = new MultipointMessageNode(port);
+			
+			this.available = new Available();
+			this.remote = new Remote(this.multipointServer, Parameters.engine, available);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-
-	@Override
-	public int getLocalPort() {
-		return this.port;
-	}
-
-	@Override
-	public List<MessageChannel> getChannels() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MessageChannel addChannel(String host, int port) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public MessageChannel addChannel(InetSocketAddress address) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 }
